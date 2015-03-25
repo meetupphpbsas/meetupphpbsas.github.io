46dfcc29df3b6ab52d15fb3e2f4f4c7df7a6d57e
Andres Lozada Mosto <alfathenus@gmail.com>
2015-03-25 07:39:12 -0300

    Primer commit con la web

---
+++ main.js
@@ -0,0 +1,67 @@
+$(function() {
+    var data = {
+        count_past_zero: false,
+        circle_bg_color: "#3D7B94",
+        fg_width: 0.02,
+        bg_width: 0.2,
+        time: {
+
+            Days: {
+                text: "Dias",
+                color: "#3D7B94",
+                show: true
+            },
+            Hours: {
+                text: "Horas",
+                color: "#3D7B94",
+                show: true
+            },
+            Minutes: {
+                text: "Minutos",
+                color: "#3D7B94",
+                show: true
+            },
+            Seconds: {
+                text: "Segundos",
+                color: "#3D7B94",
+                show: true
+            }
+        }
+    };
+
+    $(".countdownClock").TimeCircles(data);
+
+    var infowindow = new google.maps.InfoWindow({
+        content: "<b>IT Master</b><br>Av Rivadavia 5040, Buenos Aires."
+    });
+    var myLatlng = new google.maps.LatLng(-34.6185322, -58.4369542);
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        draggable: false, 
+        scrollwheel: false, 
+        disableDoubleClickZoom: true,
+        zoomControl: true,
+        streetViewControl:true,
+        panControl: true,
+        panControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_TOP
+        },
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.SMALL,
+            position: google.maps.ControlPosition.RIGHT_BOTTOM
+        },
+        streetViewControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_BOTTOM
+        }
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'IT Master'
+    });
+    infowindow.open(map,marker);
+});

441e1134aa9ac22223d93dc47c119008d6ff01eb
Federico Mosto <mosto.federico@gmail.com>
2015-11-07 18:45:31 -0300

    Parallax

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,79 +1,65 @@
 $(function() {
-    
-    // Clock
-    var data = {
-        count_past_zero: false,
-        //circle_bg_color: "#3D7B94",
-        circle_bg_color: "#f5f5f5",
-        fg_width: 0.02,
-        bg_width: 0.2,
-        time: {
-
-            Days: {
-                text: "Dias",
-                //color: "#3D7B94",
-                color: "#f5f5f5",
-                show: true
-            },
-            Hours: {
-                text: "Horas",
-                //color: "#3D7B94",
-                color: "#f5f5f5",
-                show: true
-            },
-            Minutes: {
-                text: "Minutos",
-                //color: "#3D7B94",
-                color: "#f5f5f5",
-                show: true
-            },
-            Seconds: {
-                text: "Segundos",
-                //color: "#3D7B94",
-                color: "#f5f5f5",
-                show: true
-            }
-        }
-    };
-    $(".countdownClock").TimeCircles(data);
-
-    $(window).resize(function(){
-        $(".countdownClock").TimeCircles().rebuild();
-    });
-
-    
-    var infowindow = new google.maps.InfoWindow({
-        content: "<span class='infowin'><b>IT Master</b><br>Av Rivadavia 5040, Buenos Aires.</span>"
-    });
-    var myLatlng = new google.maps.LatLng(-34.6185322, -58.4369542);
-    var mapOptions = {
-        center: myLatlng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        mapTypeControl: false,
-        draggable: false, 
-        scrollwheel: false, 
-        disableDoubleClickZoom: true,
-        zoomControl: true,
-        streetViewControl:true,
-        panControl: true,
-        panControlOptions: {
-            position: google.maps.ControlPosition.RIGHT_TOP
-        },
-        zoomControlOptions: {
-            style: google.maps.ZoomControlStyle.SMALL,
-            position: google.maps.ControlPosition.RIGHT_BOTTOM
-        },
-        streetViewControlOptions: {
-            position: google.maps.ControlPosition.RIGHT_BOTTOM
-        }
-    };
-    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-    /*var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        title: 'IT Master'
+    // Init Skrollr
+    /*var sk = skrollr.init({
+        smoothScrolling: false,
+        mobileDeceleration: 0.004
     });*/
-    infowindow.open(map,marker);
+
+
+
+
+
+
+
+
+
+    // Setup variables
+    $window = $(window);
+    $slide = $('.homeSlide');
+    $slideTall = $('.homeSlideTall');
+    $slideTall2 = $('.homeSlideTall2');
+    $body = $('body');
+
+    //FadeIn all sections
+    $body.imagesLoaded( function() {
+        setTimeout(function() {
+
+              // Resize sections
+              adjustWindow();
+
+              // Fade in sections
+              $body.removeClass('loading').addClass('loaded');
+
+        }, 800);
+    });
+
+    function adjustWindow(){
+
+        // Init Skrollr
+        var s = skrollr.init({
+            render: function(data) {
+
+                //Debugging - Log the current scroll position.
+                //console.log(data.curTop);
+            }
+        });
+
+        // Get window size
+        winH = $window.height();
+
+        // Keep minimum height 550
+        if(winH <= 550) {
+            winH = 550;
+        }
+
+        // Resize our slides
+        $slide.height(winH);
+        $slideTall.height(winH*2);
+        $slideTall2.height(winH*3);
+
+        // Refresh Skrollr after resizing our sections
+        s.refresh($('.homeSlide'));
+
+    }
     
 });
---
+++ assets/js/main_old.js
@@ -0,0 +1,79 @@
+$(function() {
+    
+    // Clock
+    var data = {
+        count_past_zero: false,
+        //circle_bg_color: "#3D7B94",
+        circle_bg_color: "#f5f5f5",
+        fg_width: 0.02,
+        bg_width: 0.2,
+        time: {
+
+            Days: {
+                text: "Dias",
+                //color: "#3D7B94",
+                color: "#f5f5f5",
+                show: true
+            },
+            Hours: {
+                text: "Horas",
+                //color: "#3D7B94",
+                color: "#f5f5f5",
+                show: true
+            },
+            Minutes: {
+                text: "Minutos",
+                //color: "#3D7B94",
+                color: "#f5f5f5",
+                show: true
+            },
+            Seconds: {
+                text: "Segundos",
+                //color: "#3D7B94",
+                color: "#f5f5f5",
+                show: true
+            }
+        }
+    };
+    $(".countdownClock").TimeCircles(data);
+
+    $(window).resize(function(){
+        $(".countdownClock").TimeCircles().rebuild();
+    });
+
+    
+    var infowindow = new google.maps.InfoWindow({
+        content: "<span class='infowin'><b>IT Master</b><br>Av Rivadavia 5040, Buenos Aires.</span>"
+    });
+    var myLatlng = new google.maps.LatLng(-34.6185322, -58.4369542);
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        draggable: false, 
+        scrollwheel: false, 
+        disableDoubleClickZoom: true,
+        zoomControl: true,
+        streetViewControl:true,
+        panControl: true,
+        panControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_TOP
+        },
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.SMALL,
+            position: google.maps.ControlPosition.RIGHT_BOTTOM
+        },
+        streetViewControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_BOTTOM
+        }
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    /*var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'IT Master'
+    });*/
+    infowindow.open(map,marker);
+    
+});

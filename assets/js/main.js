1272402a6b5abb7585cf6784519a6a5b83b65b29
Federico Mosto <mosto.federico@gmail.com>
2015-11-25 17:42:40 -0300

    Se cambia el codigo del paralax y se agregan nuevas secciones. El diseño gral se mantiene

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,4 +79,34 @@ $(function() {
         title: 'IT Master'
     });
     infowindow.open(map,marker);
+
+
+    /** ****************************************
+     * Meetups
+     **************************************** */
+
+
+    // add the animation to the modal
+    $( ".modal" ).each(function(index) {
+        $(this).on('show.bs.modal', function (e) {
+            var open = $(this).attr('data-easein');
+            
+            if(open == 'shake') {
+                $('.modal-dialog').velocity('callout.' + open);
+            } else if(open == 'pulse') {
+                $('.modal-dialog').velocity('callout.' + open);
+            } else if(open == 'tada') {
+                $('.modal-dialog').velocity('callout.' + open);
+            } else if(open == 'flash') {
+                $('.modal-dialog').velocity('callout.' + open);
+            }  else if(open == 'bounce') {
+                $('.modal-dialog').velocity('callout.' + open);
+            } else if(open == 'swing') {
+                $('.modal-dialog').velocity('callout.' + open);
+            } else {
+                $('.modal-dialog').velocity('transition.' + open);
+            }
+        });
+    });
+
 });
